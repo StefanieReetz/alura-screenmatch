f5aee6c15655a3dafcbb3db36b14aef0b4a3c542
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2024-07-13 17:15:17 -0300

    Arrumacao dos erros, adicionado os getters e setters

--- src/Principal.java
+++ src/Principal.java
@@ -4,15 +4,16 @@ public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
         meuFilme.setNome("Meu Malvado Favorito!");
-        meuFilme.anoDeLancamento = 2010;
+        meuFilme.setAnoDeLancamento(2010);
+        meuFilme.setDuracaoEmMinutos(140);
 
         //Metodo
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(5);
         meuFilme.avalia(8);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
-        System.out.println(meuFilme.mediaDasAvaliacoes());
+        //meuFilme.somaDasAvaliacoes = 10;
+        //meuFilme.totalDeAvaliacoes = 1;
+        //System.out.println(meuFilme.mediaDasAvaliacoes());
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -9,17 +9,47 @@ public class Filme {
     private int totalDeAvaliacoes;
     private int DuracaoEmMinutos;
 
-    public void exibeFichaTecnica(){
-        System.out.println("\nNome do filme: " + nome);
-        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    public String getNome() {
+        return nome;
+    }
 
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return DuracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        DuracaoEmMinutos = duracaoEmMinutos;
     }
 
     public void setNome(String nome){
         this.nome = nome;
     }
 
+    public void exibeFichaTecnica(){
+        System.out.println("\nNome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
 
+    }
 
     public void avalia(double nota) {
         somaDasAvaliacoes += nota;
