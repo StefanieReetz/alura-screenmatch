bbada02c4907b51f2f9e507f7d94d78e246d2891
Stefanie Reetz <stefaniebeatrizreetz@gmail.com>
2024-07-13 11:26:40 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("Meu Malvado Favorito!");
+        meuFilme.anoDeLancamento = 2010;
+
+        //Metodo
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(5);
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.mediaDasAvaliacoes());
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,34 @@
+package br.com.alura.screenmatch.modelos;
+
+// Isso é um objeto
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int DuracaoEmMinutos;
+
+    public void exibeFichaTecnica(){
+        System.out.println("\nNome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+    double mediaDasAvaliacoes(){
+        //Metodo ou a maneira de
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    };
+}
+
+
